18fef0dc7a688c09d9f101b56915d68ee8c3edc3
Matheus Hernandez <matheushdz0903@gmail.com>
2023-03-28 12:57:48 -0300

    bateria v0.1

---
+++ main.js
@@ -0,0 +1,10 @@
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play()
+}
+document.querySelector('.tecla_pom')
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play()
+}
+document.querySelector('.tecla_clap')
+
