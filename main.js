a96a8e94a51f3670b94e2d1fb1cce26be1924b4e
Matheus Hernandez <matheushdz0903@gmail.com>
2023-03-31 08:56:36 -0300

    Problema no listaDeTeclas

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-const tocaSom = document.querySelectorAll('#som').play()
-const listaDeTeclas = document.querySelectorAll('.tecla')
-
-for(var count = 0; count <= listaDeTeclas.length; count++){
-    listaDeTeclas[count].onclick = tocaSom[count]
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+
+// for(let count = 0; count <= listaDeTeclas.length; count++){
+//     const tecla = listaDeTeclas[count]
+//     const instrumento = tecla.classList[1]
+//     console.log(instrumento)
+
+//     tecla.classList[1] = som[count].classList[1].play()
+     
+// }
+// let count = 0
+
+// while (count < listaDeTeclas.length) {
+//     const tecla = listaDeTeclas[count];
+
+//     const instrumento = tecla.classList[1];
+
+//     console.log(instrumento)
+
+//     const idAudio = `#som_${instrumento}`
+
+//     tecla.onclick = function() {
+//         som(idAudio)
+//     };
+
+//     count++
+    
+// }
